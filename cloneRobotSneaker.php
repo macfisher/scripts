cc9d5b3cffbe9226cdb093cc84b0aec39a5882e5
macfisher <macfisher01@gmail.com>
2015-04-11 16:08:21 -0600

    Minor tweak to clone robot-sneaker-crud script

--- cloneRobotSneaker.php
+++ cloneRobotSneaker.php
@@ -1,12 +1,12 @@
 <?php
 
 //clone robot-sneaker repo
-$cloneCmd = sprintf("sudo -u root git clone https://github.com/macfisher/robot-sneaker");
-$cloneExe = shell_exec($cloneCmd);
+$cloneCmd = sprintf("sudo -u root git clone https://github.com/macfisher/robot-sneaker-crud");
 
 //set permissions
-$wwwDataCmd = sprintf("sudo -u root chown -R :www-data /var/www/robot-sneaker");
-$wwwDataExe = shell_exec($wwwDataCmd);
+$wwwDataCmd = sprintf("sudo -u root chown -R :www-data /var/www/robot-sneaker-crud");
+$storageCmd = sprintf("sudo -u root chmod -R 775 /var/www/robot-sneaker-crud/app/storage");
 
-$storageCmd = sprintf("sudo -u root chmod -R 775 /var/www/robot-sneaker/app/storage");
+$cloneExe = shell_exec($cloneCmd);
+$wwwDataExe = shell_exec($wwwDataCmd);
 $storageExe = shell_exec($storageCmd);
